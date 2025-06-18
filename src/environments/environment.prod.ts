2e0fd0cd9698e441067a5f1b8ec89b99f3e0f01c
FR33Zz3 <160392241+FR33Zz3@users.noreply.github.com>
2025-06-18 04:52:05 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://85.235.205.223:8080/api/sign'  // URL вашего API на Render
+  apiUrl: 'localhost:8080'  // URL вашего API на Render
 };
